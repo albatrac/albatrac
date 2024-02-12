aba0edb10789b4f18f6bf5d9d62ee35db7a1eb30
Louise Keinström <louise.keinstrom@outlook.com>
2024-02-12 18:30:51 +0100

    added navigation and page-intro

--- src/frontend/components/navigation.tsx
+++ src/frontend/components/navigation.tsx
@@ -5,16 +5,30 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink, Link } from "react-router-dom";
+import Logo from "../../assets/images/Element 8.png";
 
 const Navigation = () => {
 	return (
 		<>
 			<Navbar className="navigation">
 				<Container className="navigation__container spacing">
-					<Navbar.Brand as={Link} to="#"></Navbar.Brand>
-					<Nav>
-						<Nav.Link as={NavLink} to="#">
-							About us
+					<Navbar.Brand as={Link} to="#">
+						<img src={Logo} alt="" className="navigation__logo" />
+					</Navbar.Brand>
+					<Nav className="navigation__link-container">
+						<Nav.Link
+							as={NavLink}
+							to="#"
+							className="navigation__link-texts"
+						>
+							<h2>about us</h2>
+						</Nav.Link>
+						<Nav.Link
+							as={NavLink}
+							to="#"
+							className="navigation__link-texts"
+						>
+							<h2>contact</h2>
 						</Nav.Link>
 					</Nav>
 				</Container>
--- src/frontend/pages/landing-page.tsx
+++ src/frontend/pages/landing-page.tsx
@@ -5,8 +5,19 @@
 const LandingPage = () => {
 	return (
 		<>
-			<section>
-				<div>hej</div>
+			<section className="page-intro">
+				<div className="page-intro__container spacing">
+					<div className="page-intro__introduction">
+						<h2>
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting industry. Lorem Ipsum has been the
+							industry's standard dummy text ever since the 1500s,
+						</h2>
+						<button className="button button--dark">
+							Click here
+						</button>
+					</div>
+				</div>
 			</section>
 		</>
 	);
