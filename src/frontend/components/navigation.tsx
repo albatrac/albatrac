5db981316503eaf9b91d69fd6ec13b77870f452e
Louise Keinström <louise.keinstrom@outlook.com>
2024-02-13 13:56:07 +0100

    fixed nav, more sections in landing-page

--- src/frontend/components/navigation.tsx
+++ src/frontend/components/navigation.tsx
@@ -6,11 +6,25 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { NavLink, Link } from "react-router-dom";
 import Logo from "../../assets/images/Element 8.png";
+import { useState } from "react";
 
 const Navigation = () => {
+	const [navChange, setNavchange] = useState(false);
+	const changeNavbarColor = () => {
+		if (window.scrollY >= 80) {
+			setNavchange(true);
+		} else {
+			setNavchange(false);
+		}
+	};
+	window.addEventListener("scroll", changeNavbarColor);
 	return (
 		<>
-			<Navbar className="navigation">
+			<Navbar
+				className={
+					navChange ? "navigation" : "navigation navigation--scroll"
+				}
+			>
 				<Container className="navigation__container spacing">
 					<Navbar.Brand as={Link} to="#">
 						<img src={Logo} alt="" className="navigation__logo" />
@@ -21,14 +35,14 @@ const Navigation = () => {
 							to="#"
 							className="navigation__link-texts"
 						>
-							<h2>about us</h2>
+							<h2 className="h3">about us</h2>
 						</Nav.Link>
 						<Nav.Link
 							as={NavLink}
 							to="#"
 							className="navigation__link-texts"
 						>
-							<h2>contact</h2>
+							<h2 className="h3">contact</h2>
 						</Nav.Link>
 					</Nav>
 				</Container>
--- src/frontend/pages/landing-page.tsx
+++ src/frontend/pages/landing-page.tsx
@@ -3,6 +3,10 @@
  */
 import introImage from "../../assets/images/allan-nygren--xvKQuNtOhI-unsplash.jpg";
 import aboutImage from "../../assets/images/steven-shircliff-N21z4eG8aKg-unsplash.jpg";
+import displayImg1 from "../../assets/images/Hole_Overview.png";
+import displayImg2 from "../../assets/images/Selection_of_Course.png";
+import displayImg3 from "../../assets/images/Preview_of_Course.png";
+
 const LandingPage = () => {
 	return (
 		<>
@@ -15,11 +19,11 @@ const LandingPage = () => {
 			>
 				<div className="page-intro__container spacing">
 					<div className="page-intro__introduction">
-						<h2>
+						<h3>
 							Lorem Ipsum is simply dummy text of the printing and
 							typesetting industry. Lorem Ipsum has been the
 							industry's standard dummy text ever since the 1500s,
-						</h2>
+						</h3>
 						<button className="button button--dark">
 							Click here
 						</button>
@@ -32,6 +36,7 @@ const LandingPage = () => {
 						<img src={aboutImage} alt="" className="about__image" />
 					</div>
 					<div className="about__information">
+						<div className="line"></div>
 						<h2>About</h2>
 						<p>
 							Lorem Ipsum is simply dummy text of the printing and
@@ -44,14 +49,44 @@ const LandingPage = () => {
 						<button className="button button--dark">
 							Click here
 						</button>
+						<div className="line"></div>
 					</div>
 				</div>
 			</section>
 			<section className="players">
 				<div className="players__container spacing">
-					<h2 className="players__heading">Benefits for players</h2>
 					<div className="players__benefits-container">
+						<h2>Benefits for players</h2>
 						<ul className="players__benefits">
+							<li>● Reason Nr. 1</li>
+							<li>● Reason Nr. 2</li>
+							<li>● Reason Nr. 3</li>
+						</ul>
+					</div>
+					<div className="players__display">
+						<img
+							src={displayImg1}
+							alt=""
+							className="players__backgr-img"
+						/>
+						<img
+							src={displayImg2}
+							alt=""
+							className="players__front-img"
+						/>
+						<img
+							src={displayImg3}
+							alt=""
+							className="players__backgr-img"
+						/>
+					</div>
+				</div>
+			</section>
+			<section className="coaches">
+				<div className="coaches__container spacing">
+					<div className="coaches__benefits-container">
+						<h2>Benefits for coaches</h2>
+						<ul className="coaches__benefits">
 							<li>● Reason Nr. 1</li>
 							<li>● Reason Nr. 2</li>
 							<li>● Reason Nr. 3</li>
